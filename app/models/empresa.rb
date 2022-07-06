713224014f6d1c77015c5fbf2715f7b81ae092fa
stgoneira <stgoneiran@gmail.com>
2022-07-06 16:00:50 -0400

    ex has_many

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,4 +1,5 @@
 class Empresa < ApplicationRecord
+  has_many :postulaciones
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
